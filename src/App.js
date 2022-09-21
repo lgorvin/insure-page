baf00b2795137691376d48fba61842db4f9aa92f
luca <lgorvin78@gmail.com>
2022-09-21 17:00:55 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Mobile from "./components/Mobile";
 import CollapsibleExample from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <CollapsibleExample />
+      <Mobile />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,109 +22,31 @@ function CollapsibleExample() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto mx-8">
-            <NavDropdown
-              className="mr-8 text-md"
-              title="Features"
-              id="collasible-nav-dropdown"
-            >
-              <NavDropdown.Item
-                className="text-sm almostBlack"
-                href="#action/3.1"
-              >
-                <svg
-                  className="inline mr-5"
-                  width="14"
-                  height="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M14 3v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h1V1a1 1 0 1 1 2 0v1h2V1a1 1 0 1 1 2 0v1h2V1a1 1 0 0 1 2 0v1h1a1 1 0 0 1 1 1Zm-2 3H2v1h10V6Zm0 3H2v1h10V9Zm0 3H2v1h10v-1Z"
-                    fill="#726CEE"
-                  />
-                </svg>
-                ToDo List
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className="text-sm almostBlack"
-                href="#action/3.2"
-              >
-                <svg
-                  className="inline mr-5"
-                  width="16"
-                  height="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.667 8.667h-4v4h4v-4ZM11.334 0v1.333H4.667V0h-2v1.333h-1C.75 1.333 0 2.083 0 3v11.333C0 15.25.75 16 1.667 16h12.667C15.25 16 16 15.25 16 14.333V3c0-.917-.75-1.667-1.666-1.667h-1V0h-2Zm3 14.333H1.667V5.5h12.667v8.833Z"
-                    fill="#4BB1DA"
-                  />
-                </svg>
-                Calendar
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className="text-sm almostBlack"
-                href="#action/3.3"
-              >
-                <svg
-                  className="inline mr-5"
-                  width="13"
-                  height="17"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6.408 13.916c-3.313 0-6-1.343-6-3 0-.612.371-1.18 1-1.654V7.916a5 5 0 0 1 3.041-4.6 2 2 0 0 1 3.507-1.664 2 2 0 0 1 .411 1.664 5.002 5.002 0 0 1 3.041 4.6v1.346c.629.474 1 1.042 1 1.654 0 1.657-2.687 3-6 3Zm0 1c.694 0 1.352-.066 1.984-.16.004.054.016.105.016.16a2 2 0 0 1-4 0c0-.055.012-.106.016-.16.633.094 1.29.16 1.984.16Z"
-                    fill="#EDD556"
-                  />
-                </svg>
-                Reminders
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className="text-sm almostBlack"
-                href="#action/3.4"
-              >
-                <svg
-                  className="inline mr-5"
-                  width="16"
-                  height="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0Zm0 2.133a5.867 5.867 0 1 0 0 11.734A5.867 5.867 0 0 0 8 2.133ZM8 3.2a4.8 4.8 0 1 1 0 9.6 4.8 4.8 0 0 1 0-9.6Zm-.533 2.667a.533.533 0 0 0-.534.533v2.133c0 .295.24.534.534.534h3.2a.533.533 0 0 0 0-1.067H8V6.4a.533.533 0 0 0-.533-.533Z"
-                    fill="#8E4CB6"
-                  />
-                </svg>
-                Planning
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown
-              className="mr-8 text-md"
-              title="Company"
-              id="collasible-nav-dropdown"
-            >
-              <NavDropdown.Item href="#action/3.1">History</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Our Team</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Blog</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className="almostBlack mr-8 text-md" href="#careers">
-              Careers
-            </Nav.Link>
-            <Nav.Link className="almostBlack text-md" href="#about">
-              About
-            </Nav.Link>
-          </Nav>
+          <Nav className="me-auto mx-0"></Nav>
           <Nav>
             <Nav.Link
-              className="mr-8 text-md  text-center lg:ml-0"
+              className="mr-0 text-xl mt-8 lg:mt-0 lg:text-sm  text-center lg:ml-0"
               href="#deets"
             >
-              Login
+              HOW WE WORK
             </Nav.Link>
             <Nav.Link
               href="#deets"
-              className="text-md border-2 text-center  lg:ml-0 border-gray-400 active:border-gray-900 lg:px-4 rounded-2xl"
+              className="text-xl lg:text-sm mt-2 lg:mt-0   text-center  lg:ml-0 border-gray-400 active:border-gray-900  rounded-2xl"
             >
-              Register
+              BLOG
+            </Nav.Link>
+            <Nav.Link
+              className="mr-0 lg:mr-2 text-xl lg:text-sm mt-2 lg:mt-0  text-center lg:ml-0"
+              href="#deets"
+            >
+              ACCOUNT
+            </Nav.Link>
+            <Nav.Link
+              className="mr-0 lg:mr-16 text-xl lg:text-xs mt-2 lg:mt-0 font-bold lg:border-2 border-black text-black px-4 py-2  text-center lg:ml-0"
+              href="#deets"
+            >
+              VIEW PLANS
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
